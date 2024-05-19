b95de2694c480558d40aaec1ea0963dabf629c9d
Shaina Caoile <163720107+shainacaoile@users.noreply.github.com>
2024-05-19 04:25:21 -0300

    Add files via upload

---
+++ SIGN-IN&SIGN-UP/sign-up.php
@@ -0,0 +1,7 @@
+<?php
+    echo "{$_POST["fullname"]} <br>";
+    echo "{$_POST["email"]} <br>";
+    echo "{$_POST["password"]} <br>";
+    echo "{$_POST["address"]} <br>";
+    echo "{$_POST["user_type"]} <br>";
+?>
